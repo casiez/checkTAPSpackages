cb1bbb08365dd577400c1480ad8c29edeb5420f9
Géry Casiez <gery.casiez@univ-lille1.fr>
2021-01-07 21:43:51 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # checkTAPSpackages
+
+Allows to check an article written in LaTeX only uses standard packages listed on the [TAPS page](https://www.acm.org/publications/taps/accepted-latex-packages)
+
+Install using ```pip install checkTAPSpackages```
+
+1. Compile your document by adding ```\listfiles``` below ```\documentclass```
+1. Run ```checkTAPSpackages yourGeneratedLogFile.log```
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='checkTAPSpackages',  
+     version='0.1',
+     scripts=['checkTAPSpackages'] ,
+     author="Géry Casiez",
+     author_email="gery.casiez@univ-lille.fr",
+     description="Allows to check a document complies with the list of LaTeX packages accepted by TAPS.",
+     long_description=long_description,
+
+     long_description_content_type="text/markdown",
+     url="https://github.com/casiez/checkTAPSpackages",
+     packages=setuptools.find_packages(),
+     install_requires=[ 'argparse'],
+
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+
+ )
