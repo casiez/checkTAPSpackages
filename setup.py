60d66f6b51450e2cc6fb775f0431b69f281fcf25
Géry Casiez <gery.casiez@univ-lille1.fr>
2021-01-08 11:06:44 +0100

    Added packages included by others but not listed on the TAPS webpage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='checkTAPSpackages',  
-     version='0.1',
+     version='0.2.1',
      scripts=['checkTAPSpackages'] ,
      author="Géry Casiez",
      author_email="gery.casiez@univ-lille.fr",
