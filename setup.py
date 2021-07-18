1cf75f7377c1c163d8a28e2bebdf5a8f69c39d66
Géry Casiez <3748153+casiez@users.noreply.github.com>
2021-07-18 18:45:52 +0200

    updated packages list + added diff option to check newly supported packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='checkTAPSpackages',  
-     version='0.4.1',
+     version='0.5.0',
      scripts=['checkTAPSpackages'] ,
      author="Géry Casiez",
      author_email="gery.casiez@univ-lille.fr",
